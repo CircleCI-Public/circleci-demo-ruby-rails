80cdb2de6a0bac40520e48774d18b7dff05fb7e8
ganezasan <istaka12@gmail.com>
2019-04-19 12:30:58 +0900

    update root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
   get 'welcome/index'
+  root :to => 'posts#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
