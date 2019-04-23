c4b066a46c53d756562a6b54240eec83ac3522e6
Manh Nguyen <manhng.201@gmail.com>
2019-04-23 21:32:55 +0700

    Remove some spec

--- spec/helpers/posts_helper_spec.rb
+++ spec/helpers/posts_helper_spec.rb
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe PostsHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe PostsHelper, type: :helper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
