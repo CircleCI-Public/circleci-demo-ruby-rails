0792a2e4f7c740f93f7c3e2de0cb2e8d2cea9f1b
Rodrigo Souto <rodrigo.souto@clark.de>
2020-04-24 11:47:38 +0200

    Break tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "Posts", type: :request do
   describe "GET /posts" do
     it "works! (now write some real specs)" do
       get posts_path
-      expect(response).to have_http_status(200)
+      expect(response).to have_http_status(201)
     end
   end
 end
